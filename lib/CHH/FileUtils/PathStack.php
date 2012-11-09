f08f89cf0b77f6c419aa02ec56e286d4132495eb
Romain Dardour <romain@unity.fr>
2012-11-09 18:31:13 +0100

    Allow a second argument in PathStack.find : a string of an additional directory to search in or an array of such

--- lib/CHH/FileUtils/PathStack.php
+++ lib/CHH/FileUtils/PathStack.php
@@ -84,23 +84,42 @@ class PathStack
         return $this;
     }
 
-    function find($file)
+    function find($file, $dirs=null)
     {
-        foreach ($this->paths() as $loadPath) {
-            $path = Path::join(array($loadPath, $file));
+        if(is_string($dirs)){
+            $dirs = array($dirs);
+        }
 
-            if (file_exists($path)) {
-                return $path;
-            } else {
-                foreach ($this->extensions() as $ext) {
-                    if (file_exists($path . $ext)) {
-                        return $path . $ext;
-                    }
+        if(is_array($dirs) && count($dirs)){
+            foreach ($dirs as $loadPath){
+                $p = $this->find_file(rtrim($loadPath,'\/'), $file);
+                if($p){
+                    return $p;
                 }
+            }
+        }
+        foreach ($this->paths() as $loadPath){
+            $p = $this->find_file($loadPath, $file);
+            if($p){
+                return $p;
             }
         }
 
         return false;
+    }
+
+    function find_file($loadPath, $file){
+        $path = Path::Join( array( $loadPath,$file ) );
+        if (file_exists($path)) {
+            return $path;
+        } else {
+            foreach ($this->extensions() as $ext) {
+                if (file_exists($path . $ext)) {
+                    return $path . $ext;
+                }
+            }
+        }
+        return false;        
     }
 
     function __toString()
